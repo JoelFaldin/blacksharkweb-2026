dbf1649b6885097754e06a78b71c87532b6adc7d
JoelFaldin <joelfaldin@gmail.com>
2025-11-17 23:23:57 -0300

    feat: adding background effect component and using it on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { BackgroundEffect } from "@/components/background/BackgroundEffect"
 
 export default function Home() {
   return (
     <>
+      <BackgroundEffect />
       <div>Home</div>
     </>
   )
