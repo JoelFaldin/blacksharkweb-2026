c9f239fac3d4166fd405db00aef18dc14746df32
SamuelSotomayor1 <andressotomayor477@gmail.com>
2026-01-19 23:39:34 -0300

    feat: navbar with username logged working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,45 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
 import { BackgroundEffect } from "@/components/background/BackgroundEffect";
+import { createSupabaseServerClient } from "@/lib/supabase/server-client";
 
 export const metadata: Metadata = {
   title: {
     template: "%s | BlackSharkWeb",
     default: "BlackSharkWeb",
   },
-  description: "Lleva el diseño de tu marca al siguiente nivel, revisa los servicios disponibles y transforma la imagen de tu empresa en BlackSharkWeb!",
+  description:
+    "Lleva el diseño de tu marca al siguiente nivel, revisa los servicios disponibles y transforma la imagen de tu empresa en BlackSharkWeb!",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+
+  const supabase = await createSupabaseServerClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const isAuthenticated = !!user;
+
+  const userName =
+    user?.user_metadata?.full_name ||
+    user?.email?.split("@")[0] ||
+    undefined;
+
   return (
-    <html lang="en">
+    <html lang="es">
       <body>
         <BackgroundEffect />
-        <Navbar />
+
+        <Navbar
+          isAuthenticated={isAuthenticated}
+          userName={userName}
+        />
 
         {children}
       </body>
