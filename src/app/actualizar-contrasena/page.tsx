25c70b52eb9f21a3a70423ae86879e32dace20b3
SamuelSotomayor1 <andressotomayor477@gmail.com>
2026-02-28 00:52:17 -0300

    feat: the reset password with supabase are agreggated

---
+++ src/app/actualizar-contrasena/page.tsx
@@ -0,0 +1,9 @@
+import ActualizarContrasena from "./ActualizarContrasena";
+
+export default function ActualizarContrasenaPage() {
+  return (
+    <div className="min-h-[80vh] flex items-center justify-center px-6 py-24">
+        <ActualizarContrasena />
+    </div>
+  );
+}
